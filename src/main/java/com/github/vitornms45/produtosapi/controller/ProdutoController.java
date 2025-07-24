2737b889352f315c92a62f4130cac4fc226fba58
Victor Nunes Moraes Silva <vitornones25@gmail.com>
2025-07-24 17:45:07 -0300

    Adição de lógica para salvamento de produtos no BD

--- src/main/java/com/github/vitornms45/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/vitornms45/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,30 @@
 package com.github.vitornms45.produtosapi.controller;
 
 import com.github.vitornms45.produtosapi.model.entity.Product;
+import com.github.vitornms45.produtosapi.repository.entity.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping("")
     public Product salvar(@RequestBody Product produto) {
         System.out.println("Salvando produto: " + produto);
+        UUID uuid = UUID.randomUUID();
+        produto.setId(uuid.toString());
+        produtoRepository.save(produto);
         return produto;
     }
 }
