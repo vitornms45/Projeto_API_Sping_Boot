693ea7a803cd0f7c6fcc9f8bfe97eee78b75fbfa
Victor Nunes Moraes Silva <vitornones25@gmail.com>
2025-09-08 16:02:02 -0300

    Adição de delete

--- src/main/java/com/github/vitornms45/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/vitornms45/produtosapi/controller/ProdutoController.java
@@ -32,4 +32,9 @@ public class ProdutoController {
     //    return product.isPresent() ? product.get() : null;
         return produtoRepository.findById(id).orElse(null);
     }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
 }
